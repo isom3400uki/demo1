1c5070e8bc38a33c8aac0f4b6d733fe6e42b6e13
isom3400uki <yukiwong3553@gmail.com>
2025-11-18 15:52:21 +0800

    Update labdemo.py

--- labdemo.py
+++ labdemo.py
@@ -4,8 +4,36 @@ import matplotlib.pyplot as plt
 
 st.title("Personal Expense Treacker")
 
-st. title("Personal Expense Tracker")
 if 'expenses' not in st.session_state:
-    st. session_state.expenses • pd.DataFrame(columns«['Date','Category','Amount', 'Description' ))
+    st.session_state.expenses = pd.DataFrame(columns= ['Date','Category','Amount', 'Description'])
+    
 with st. form("expense_form"):
-    st. subheader ("Add New Expense")
+    st.subheader ("Add New Expense")
+    date = st.date_input("Date")
+    category = st.selectbox(
+
+submitted = st.form_submit_button("Add Expense")
+if submitted:
+    new_expense = pd.DataFrame({
+        'Date': [Date],
+        'Category': [Category],
+        'Amount': [Amount],
+        'Description: [\Desription]
+    })
+    st.session_state.expenses = pd. concat([st.session_state.expenses, new_expense], ignore_index=True)
+    st.success("Expense added successfully!")
+
+if not st.session_state.expense.empty
+    st.subheader("Your Expenses")
+    st.dataframe(st.session_state.expenses)
+
+    st.subheader("Summary")
+    total_spent = st.session_state.expense['Amount'].sum()
+    st.write(f"Total Spent: ${total_spent:.2f}")
+
+    category_totals = st.session_state.expense.groupby('Category')[Amount].sum()
+    
+    fig, ax = plt.subplots(figsize=(10,6))
+    ax.pie(category_totals.values, labels=category_totals.index, autopct= '
+else:
+    st.info("No expenses recorded yet")
