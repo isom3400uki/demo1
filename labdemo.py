65db4e1d0197335d0225354f2b97a6614c70cc98
isom3400uki <yukiwong3553@gmail.com>
2025-11-18 15:28:44 +0800

    Update labdemo.py

--- labdemo.py
+++ labdemo.py
@@ -1,5 +1,7 @@
 import streamlit as st
-import pandas as pt
-import matplotlib.pilot as plt
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.title("
+st.title("Personal Expense Treacker")
+
+if 
