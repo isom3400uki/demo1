4b9375017ca25ced20f9b778b9e7b5abe41fa90d
isom3400uki <yukiwong3553@gmail.com>
2025-11-21 10:12:43 +0800

    Update 21-11-2025demo.py

--- 21-11-2025demo.py
+++ 21-11-2025demo.py
@@ -103,7 +103,7 @@ import streamlit as st
 
 option = st.selectbox(
     "How would you like to be contacted?",
-    {"Email", "Home phone", "Mobile phone"},
+    ("Email", "Home phone", "Mobile phone"),
 )
 
 st.write("You selected:", option)
